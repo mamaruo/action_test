a29383b675f39265db0b4c3f473dff74d5e1c2d8
mamaruo <37480168+mamaruo@users.noreply.github.com>
2023-06-23 20:31:44 +0800

    1

---
+++ pr-size-labeler.js
@@ -0,0 +1,12 @@
+const { execSync } = require('child_process');
+
+function countPRSize() {
+  const diffCommand = 'git diff --stat FETCH_HEAD $(git merge-base FETCH_HEAD origin/main)';
+  const diffOutput = execSync(diffCommand).toString();
+  const linesOfCode = diffOutput.split(',').pop().split(' ')[1];
+  return parseInt(linesOfCode, 10);
+}
+
+const prSize = countPRSize();
+console.log(`PR Size: ${prSize}`);
+console.log(`::set-env name=PR_SIZE::${prSize}`);
